38db1db058590b6adf85cede53f949f116422b59
Nguyen Duc Cong <nguyenduccong2005@gmail.com>
2024-10-23 12:59:32 +0700

    Thêm class notification.
    - tạo thông báo cho member.

--- src/main/java/com/example/proj/SystemTestDrive.java
+++ src/main/java/com/example/proj/SystemTestDrive.java
@@ -1,12 +1,20 @@
 package com.example.proj;
 
+import com.example.proj.Models.AccountStatus;
+import com.example.proj.Models.Member;
+import com.example.proj.Models.Notification;
+import com.example.proj.Models.NotificationBox;
+
 import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
 public class SystemTestDrive {
     public static void main(String[] args) throws ParseException {
-        CmdLine cmdLine = new CmdLine();
-        cmdLine.run();
+//        CmdLine cmdLine = new CmdLine();
+//        cmdLine.run();
+        Member member = new Member("cong", AccountStatus.ACTIVE, "congdz",
+                2 ,100);
+        member.showNotifications();
     }
 }
