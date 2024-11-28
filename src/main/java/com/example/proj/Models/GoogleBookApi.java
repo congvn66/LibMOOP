ade02f8d48a62f95932aafc78fe732f021d71c0d
Nguyen Duc Cong <nguyenduccong2005@gmail.com>
2024-11-28 11:28:45 +0700

    Thêm tìm kiếm bằng tên sách và tác giả cho API.
    - thêm các method tìm kiếm trong GoogleBookApi.

--- src/main/java/com/example/proj/Models/GoogleBookApi.java
+++ src/main/java/com/example/proj/Models/GoogleBookApi.java
@@ -6,14 +6,162 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class GoogleBookApi {
+    public static List<BookItem> getBookDetailsByAuthor(String author) {
+        List<BookItem> bookItems = new ArrayList<>();
+        try {
+            String apiUrl = "https://www.googleapis.com/books/v1/volumes?q=inauthor:" + author.replace(" ", "+");
+            URL url = new URL(apiUrl);
 
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/json");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            JSONObject jsonResponse = new JSONObject(response.toString());
+            JSONArray items = jsonResponse.optJSONArray("items");
+            if (items != null) {
+                for (int i = 0; i < items.length(); i++) {
+                    JSONObject item = items.getJSONObject(i);
+                    JSONObject volumeInfo = item.getJSONObject("volumeInfo");
+
+                    String isbn = getISBN(volumeInfo);
+                    String bookTitle = volumeInfo.optString("title", "N/A");
+                    String authors = getAuthors(volumeInfo);
+                    String publisher = volumeInfo.optString("publisher", "N/A");
+                    String publishedDate = volumeInfo.optString("publishedDate", "N/A");
+                    String subject = getSubject(volumeInfo);
+                    String language = volumeInfo.optString("language", "No language available");
+                    String numberOfPages = volumeInfo.optString("pageCount", "No number of pages available");
+                    String authorDescription = "Amazing writer";
+                    String id = null;
+                    boolean isReferenceOnly = false;
+                    double price = 0.0;
+                    BookFormat format = BookFormat.PAPERBACK;
+                    BookStatus status = BookStatus.AVAILABLE;
+                    Date dateOfPurchase = new Date();
+                    Date publicationDate = parsePublicationDate(volumeInfo);
+                    int number = 1;
+                    String location = "Unknown";
+                    String imgName = getImageUrl(volumeInfo);
+
+                    bookItems.add(new BookItem(
+                            isbn,
+                            bookTitle,
+                            subject,
+                            publisher,
+                            language,
+                            numberOfPages,
+                            authors,
+                            authorDescription,
+                            id,
+                            isReferenceOnly,
+                            price,
+                            format,
+                            status,
+                            dateOfPurchase,
+                            publicationDate,
+                            number,
+                            location,
+                            imgName
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bookItems;
+    }
+
+
+    public static List<BookItem> getBookDetailsByTitle(String title) {
+        List<BookItem> bookItems = new ArrayList<>();
+        try {
+            String apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + title.replace(" ", "+");
+            URL url = new URL(apiUrl);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/json");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            JSONObject jsonResponse = new JSONObject(response.toString());
+            JSONArray items = jsonResponse.optJSONArray("items");
+            if (items != null) {
+                for (int i = 0; i < items.length(); i++) {
+                    JSONObject item = items.getJSONObject(i);
+                    JSONObject volumeInfo = item.getJSONObject("volumeInfo");
+
+                    String isbn = getISBN(volumeInfo);
+                    String bookTitle = volumeInfo.optString("title", "N/A");
+                    String authors = getAuthors(volumeInfo);
+                    String publisher = volumeInfo.optString("publisher", "N/A");
+                    String publishedDate = volumeInfo.optString("publishedDate", "N/A");
+                    String subject = getSubject(volumeInfo);
+                    String language = volumeInfo.optString("language", "No language available");
+                    String numberOfPages = volumeInfo.optString("pageCount", "No number of pages available");
+                    String authorDescription = "Amazing writer";
+                    String id = null;
+                    boolean isReferenceOnly = false;
+                    double price = 0.0;
+                    BookFormat format = BookFormat.PAPERBACK;
+                    BookStatus status = BookStatus.AVAILABLE;
+                    Date dateOfPurchase = new Date();
+                    Date publicationDate = parsePublicationDate(volumeInfo);
+                    int number = 1;
+                    String location = "Unknown";
+                    String imgName = getImageUrl(volumeInfo);
+
+
+                    bookItems.add(new BookItem(
+                            isbn,
+                            bookTitle,
+                            subject,
+                            publisher,
+                            language,
+                            numberOfPages,
+                            authors,
+                            authorDescription,
+                            id,
+                            isReferenceOnly,
+                            price,
+                            format,
+                            status,
+                            dateOfPurchase,
+                            publicationDate,
+                            number,
+                            location,
+                            imgName
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bookItems;
+    }
 
 
     public static BookItem getBookDetailsByISBN(String isbn) {
@@ -36,13 +184,11 @@ public class GoogleBookApi {
                 }
                 reader.close();
 
-                // Parse JSON Response
                 JSONObject jsonResponse = new JSONObject(response.toString());
                 if (jsonResponse.has("items")) {
                     JSONArray items = jsonResponse.getJSONArray("items");
                     JSONObject bookInfo = items.getJSONObject(0).getJSONObject("volumeInfo");
 
-                    // Lấy các thông tin từ bookInfo
                     String title = bookInfo.optString("title", "No title available");
                     String publisher = bookInfo.optString("publisher", "No publisher available");
                     String subject = getSubject(bookInfo);
@@ -61,7 +207,6 @@ public class GoogleBookApi {
                     String location = "Unknown";
                     String imgName = getImageUrl(bookInfo);
 
-                    // Tạo đối tượng BookItem
                     BookItem book = new BookItem(isbn, title, subject, publisher, language, numberOfPages,
                             authorName, authorDescription, id, isReferenceOnly, price, format, status,
                             dateOfPurchase, publicationDate, number, location, imgName);
@@ -86,6 +231,27 @@ public class GoogleBookApi {
         }
         return "No subject available";
     }
+
+    private static String getISBN(JSONObject bookInfo) {
+        if (bookInfo.has("industryIdentifiers")) {
+            JSONArray identifiers = bookInfo.getJSONArray("industryIdentifiers");
+            for (int i = 0; i < identifiers.length(); i++) {
+                JSONObject identifier = identifiers.getJSONObject(i);
+                if (identifier.getString("type").equals("ISBN_13")) {
+                    return identifier.getString("identifier");
+                }
+            }
+            // Nếu không có ISBN-13, kiểm tra ISBN-10
+            for (int i = 0; i < identifiers.length(); i++) {
+                JSONObject identifier = identifiers.getJSONObject(i);
+                if (identifier.getString("type").equals("ISBN_10")) {
+                    return identifier.getString("identifier");
+                }
+            }
+        }
+        return "No ISBN available"; 
+    }
+
 
     private static String getAuthors(JSONObject bookInfo) {
         if (bookInfo.has("authors")) {
@@ -130,6 +296,20 @@ public class GoogleBookApi {
         BookItem book = getBookDetailsByISBN(isbn);
         if (book != null) {
             book.displayInfo();
+        }
+
+        System.out.println("-------------------------------------------");
+
+        List<BookItem> books = getBookDetailsByTitle("Harry Potter");
+        for (BookItem book1 : books) {
+            book1.displayInfo();
+        }
+
+        System.out.println("-------------------------------------------");
+
+        List<BookItem> books2 = getBookDetailsByAuthor("J. K. Rowling");
+        for (BookItem book2 : books2) {
+            book2.displayInfo();
         }
     }
 }
